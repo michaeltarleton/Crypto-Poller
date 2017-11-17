6aa3bf27522574149605e773f67df654efa95410
Michael Tarleton <michael.tarleton@me.com>
2017-11-16 21:09:10 -0600

    build-gdax: Implemented the official GDAX library

--- src/index.js
+++ src/index.js
@@ -7,11 +7,3 @@ var logger = require('./logger')
   , modules = require('./modules')
 
 modules.gdax.getAccounts()
-    .then(function(accounts){
-        logger.debug('accounts', accounts)
-    })
-    .catch(function(err){
-        if(err && !err.response) return logger.error(err)
-        var response = err.response
-        logger.error(response.status, response.data)
-    })
--- src/modules/gdax/index.js
+++ src/modules/gdax/index.js
@@ -1,9 +1,23 @@
 var settings = require('../../config')
   , logger = require([settings.appRoot, '/logger'].join(''))
   , client = require('./client')
+  , Gdax = require('gdax')
+  , authenticatedClient = new Gdax.AuthenticatedClient(settings.gdax.api.key, settings.gdax.api.secret, settings.gdax.api.passphrase, settings.gdax.api.url)
 
 var getAccounts = function(){
     return client.getPrivateRoute('GET', '/accounts')
+}
+
+var getAccounts = function(){
+    return authenticatedClient.getAccounts()
+        .then(function(accounts){
+            logger.debug(accounts, 'Accounts')
+            return accounts
+        })
+        .catch(function(err){
+            logger.error(err)
+            throw err
+        })
 }
 
 module.exports = {
