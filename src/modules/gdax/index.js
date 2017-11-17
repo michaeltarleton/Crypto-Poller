419c779b172df541c14af0602d7a0d62191e2abd
Michael Tarleton <michael.tarleton@me.com>
2017-11-17 00:07:31 -0600

    master: Added ETH and LTC to ticker email

--- src/modules/gdax/index.js
+++ src/modules/gdax/index.js
@@ -2,15 +2,13 @@ var settings = require('../../config')
   , logger = require([settings.appRoot, '/logger'].join(''))
   , Gdax = require('gdax')
   , authenticatedClient = new Gdax.AuthenticatedClient(settings.gdax.api.key, settings.gdax.api.secret, settings.gdax.api.passphrase, settings.gdax.api.url)
-  , bitcoinPublicClient = new Gdax.PublicClient('BTC-USD', settings.gdax.api.url)
+  , btcPublicClient = new Gdax.PublicClient('BTC-USD', settings.gdax.api.url)
+  , ethPublicClient = new Gdax.PublicClient('ETH-USD', settings.gdax.api.url)
+  , ltcPublicClient = new Gdax.PublicClient('LTC-USD', settings.gdax.api.url)
 
 var logError = function(err){
     logger.error(err)
     throw err
-}
-
-var getAccounts = function(){
-    return client.getPrivateRoute('GET', '/accounts')
 }
 
 var getAccounts = function(){
@@ -26,7 +24,7 @@ var getAccounts = function(){
 }
 
 var getBTCUSDTicker = function(){
-    return bitcoinPublicClient.getProductTicker()
+    return btcPublicClient.getProductTicker()
         .then(function(ticker){
             logger.debug(ticker, 'BTC-USD Ticker')
             return ticker
@@ -34,7 +32,27 @@ var getBTCUSDTicker = function(){
         .catch(logError)
 }
 
+var getETHUSDTicker = function(){
+    return ethPublicClient.getProductTicker()
+        .then(function(ticker){
+            logger.debug(ticker, 'ETH-USD Ticker')
+            return ticker
+        })
+        .catch(logError)
+}
+
+var getLTCUSDTicker = function(){
+    return ltcPublicClient.getProductTicker()
+        .then(function(ticker){
+            logger.debug(ticker, 'LTC-USD Ticker')
+            return ticker
+        })
+        .catch(logError)
+}
+
 module.exports = {
     getAccounts: getAccounts,
-    getBTCUSDTicker: getBTCUSDTicker
+    getBTCUSDTicker: getBTCUSDTicker,
+    getETHUSDTicker: getETHUSDTicker,
+    getLTCUSDTicker: getLTCUSDTicker
 }
--- src/schedules/index.js
+++ src/schedules/index.js
@@ -3,37 +3,53 @@ var settings = require('../config')
   , cron = require('node-cron')
   , modules = require('../modules')
   , emailer = require('../email')
+  , Promise = require('bluebird')
+  , moment = require('moment')
 
-function getBTCUSDTicker(){
-    var subject = 'BTC-USD Ticker Update'
-    modules.gdax.getBTCUSDTicker()
-        .then(function(ticker){
-            var msg = [
-                '\nbid: $',ticker.bid,
-                '\nask: $',ticker.ask,
-                '\ntime: ',ticker.time
-            ].join('')
-            emailer.sendBasicEmail(subject, msg)
-                .then(function(response){
-                    logger.info('successfully sent the email for ' + subject)
-                })
-                .catch(function(err){
-                    logger.error('failed to send the email for ' + subject)
-                })
-        })
-        .catch(function(err){
-            logger.debug(err)
-            emailer.sendBasicEmail(subject, 'failed to send ticker updates')
-                .then(function(response){
-                    logger.info('successfully sent the error email for ' + subject)
-                })
-                .catch(function(err){
-                    logger.error('failed to send the error email for ' + subject)
-                })
-        })
+function getGDAXTicker(){
+    var subject = 'GDAX Ticker Update'
+    Promise.all([
+        modules.gdax.getBTCUSDTicker(),
+        modules.gdax.getETHUSDTicker(),
+        modules.gdax.getLTCUSDTicker()
+    ])
+    .spread(function(btc, eth, ltc){
+
+        var msg = []
+
+        msg = [
+            '\nBTC: ',
+            '$',btc.bid,
+            ' / $',btc.ask,
+            '\nETH: ',
+            '$',eth.bid,
+            ' / $',eth.ask,
+            '\nLTC: ',
+            '$',ltc.bid,
+            ' / $',ltc.ask,
+            '\ntime: ',moment(ltc.time).format('HH:mm:ss')
+        ].join('')
+
+        emailer.sendBasicEmail(subject, msg)
+            .then(function(response){
+                logger.info('successfully sent the email for ' + subject)
+            })
+            .catch(function(err){
+                logger.error('failed to send the email for ' + subject)
+            })
+    })
+    .catch(function(err){
+        logger.debug(err)
+        emailer.sendBasicEmail(subject, 'failed to send ticker updates')
+            .then(function(response){
+                logger.info('successfully sent the error email for ' + subject)
+            })
+            .catch(function(err){
+                logger.error('failed to send the error email for ' + subject)
+            })
+    })
 }
 
-
 module.exports = function(){
-    cron.schedule('* */15 * * * *', getBTCUSDTicker)
+    cron.schedule('0 */15 * * * *', getGDAXTicker)
 }
